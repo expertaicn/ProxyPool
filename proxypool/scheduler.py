c582126880ba52f14e7081a896806e217f5f9e2f
hellozhaojian <290169713@qq.com>
2024-01-08 18:38:37 +0800

    fuck bug

--- proxypool/scheduler.py
+++ proxypool/scheduler.py
@@ -3,9 +3,33 @@ import multiprocessing
 from proxypool.processors.server import app
 from proxypool.processors.getter import Getter
 from proxypool.processors.tester import Tester
-from proxypool.setting import APP_PROD_METHOD_GEVENT, APP_PROD_METHOD_MEINHELD, APP_PROD_METHOD_TORNADO, CYCLE_GETTER, CYCLE_TESTER, API_HOST, \
-    API_THREADED, API_PORT, ENABLE_SERVER, IS_PROD, APP_PROD_METHOD, \
-    ENABLE_GETTER, ENABLE_TESTER, IS_WINDOWS
+from proxypool.setting import (
+    REDIS_CONNECTION_STRING,
+    REDIS_HOST,
+    REDIS_PORT,
+    REDIS_PASSWORD,
+    REDIS_DB,
+    REDIS_KEY,
+    PROXY_SCORE_MAX,
+    PROXY_SCORE_MIN,
+    PROXY_SCORE_INIT,
+)
+from proxypool.setting import (
+    APP_PROD_METHOD_GEVENT,
+    APP_PROD_METHOD_MEINHELD,
+    APP_PROD_METHOD_TORNADO,
+    CYCLE_GETTER,
+    CYCLE_TESTER,
+    API_HOST,
+    API_THREADED,
+    API_PORT,
+    ENABLE_SERVER,
+    IS_PROD,
+    APP_PROD_METHOD,
+    ENABLE_GETTER,
+    ENABLE_TESTER,
+    IS_WINDOWS,
+)
 from loguru import logger
 
 
@@ -15,7 +39,7 @@ if IS_WINDOWS:
 tester_process, getter_process, server_process = None, None, None
 
 
-class Scheduler():
+class Scheduler:
     """
     scheduler
     """
@@ -25,12 +49,13 @@ class Scheduler():
         run tester
         """
         if not ENABLE_TESTER:
-            logger.info('tester not enabled, exit')
+            logger.info("tester not enabled, exit")
             return
         tester = Tester()
         loop = 0
         while True:
-            logger.debug(f'tester loop {loop} start...')
+            logger.debug(f"tester loop {loop} start...")
+            logger.info(f"connect:host:{REDIS_HOST}:port:{REDIS_PORT}:password:{REDIS_PASSWORD}:db:{REDIS_DB}}")
             tester.run()
             loop += 1
             time.sleep(cycle)
@@ -40,12 +65,12 @@ class Scheduler():
         run getter
         """
         if not ENABLE_GETTER:
-            logger.info('getter not enabled, exit')
+            logger.info("getter not enabled, exit")
             return
         getter = Getter()
         loop = 0
         while True:
-            logger.debug(f'getter loop {loop} start...')
+            logger.debug(f"getter loop {loop} start...")
             getter.run()
             loop += 1
             time.sleep(cycle)
@@ -55,7 +80,7 @@ class Scheduler():
         run server for api
         """
         if not ENABLE_SERVER:
-            logger.info('server not enabled, exit')
+            logger.info("server not enabled, exit")
             return
         if IS_PROD:
             if APP_PROD_METHOD == APP_PROD_METHOD_GEVENT:
@@ -92,35 +117,34 @@ class Scheduler():
                 logger.error("unsupported APP_PROD_METHOD")
                 return
         else:
-            app.run(host=API_HOST, port=API_PORT, threaded=API_THREADED, use_reloader=False)
+            app.run(
+                host=API_HOST, port=API_PORT, threaded=API_THREADED, use_reloader=False
+            )
 
     def run(self):
         global tester_process, getter_process, server_process
         try:
-            logger.info('starting proxypool...')
+            logger.info("starting proxypool...")
             if ENABLE_TESTER:
-                tester_process = multiprocessing.Process(
-                    target=self.run_tester)
-                logger.info(f'starting tester, pid {tester_process.pid}...')
+                tester_process = multiprocessing.Process(target=self.run_tester)
+                logger.info(f"starting tester, pid {tester_process.pid}...")
                 tester_process.start()
 
             if ENABLE_GETTER:
-                getter_process = multiprocessing.Process(
-                    target=self.run_getter)
-                logger.info(f'starting getter, pid {getter_process.pid}...')
+                getter_process = multiprocessing.Process(target=self.run_getter)
+                logger.info(f"starting getter, pid {getter_process.pid}...")
                 getter_process.start()
 
             if ENABLE_SERVER:
-                server_process = multiprocessing.Process(
-                    target=self.run_server)
-                logger.info(f'starting server, pid {server_process.pid}...')
+                server_process = multiprocessing.Process(target=self.run_server)
+                logger.info(f"starting server, pid {server_process.pid}...")
                 server_process.start()
 
             tester_process and tester_process.join()
             getter_process and getter_process.join()
             server_process and server_process.join()
         except KeyboardInterrupt:
-            logger.info('received keyboard interrupt signal')
+            logger.info("received keyboard interrupt signal")
             tester_process and tester_process.terminate()
             getter_process and getter_process.terminate()
             server_process and server_process.terminate()
@@ -129,15 +153,12 @@ class Scheduler():
             tester_process and tester_process.join()
             getter_process and getter_process.join()
             server_process and server_process.join()
-            logger.info(
-                f'tester is {"alive" if tester_process.is_alive() else "dead"}')
-            logger.info(
-                f'getter is {"alive" if getter_process.is_alive() else "dead"}')
-            logger.info(
-                f'server is {"alive" if server_process.is_alive() else "dead"}')
-            logger.info('proxy terminated')
+            logger.info(f'tester is {"alive" if tester_process.is_alive() else "dead"}')
+            logger.info(f'getter is {"alive" if getter_process.is_alive() else "dead"}')
+            logger.info(f'server is {"alive" if server_process.is_alive() else "dead"}')
+            logger.info("proxy terminated")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     scheduler = Scheduler()
     scheduler.run()
